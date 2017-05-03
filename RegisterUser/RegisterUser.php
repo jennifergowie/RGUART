1c1909d25c22d2619c077633c19a8c5599aac296
jennifergowie <0308182@rgu.ac.uk>
2017-05-03 03:07:28 +0100

    Add files via upload

--- RegisterUser/RegisterUser.php
+++ RegisterUser/RegisterUser.php
@@ -2,30 +2,25 @@
 
 session_start(); // Start Session
 
-include("../AdminFiles/dbConnect.php");
-
+include("../AdminFiles/dbConnect.php");    //Establish database connection
 
 $username=$_POST["username"]; //Get username that has been entered
-//$studentName=$_POST["studentName"];
 $emailAddress=$_POST["email"]; //Get email address that has been entered
 $password=$_POST["password"]; //Get password that has been entered
 $mobileNumber=$_POST["phone"]; //Get mobile number that has been entered
-//$course =$_POST["course"];
-//$year=$_POST["year"];
 
 $sql = "INSERT INTO users (UserName, EmailAddress, Password, MobileNumber) VALUES ('".$username."','".$emailAddress."','".$password."', '".$mobileNumber."')";
 $link->query($sql);
 
+//$sql_two = "INSERT INTO userprofiles (UserName, EmailAddress, Password, MobileNumber) VALUES ('".$username."','".$emailAddress."','".$password."', '".$mobileNumber."')";
+//$link->query($sql_two);
 
-//$sql = "INSERT INTO userprofiles(UserName, StudentName, EmailAddress, Password, MobileNumber, Course, Year) VALUES ('".$username."','".$studentName."','".$emailAddress."','".$password."','".$mobileNumber."', '".$course."','".$year."')";
-//$link->query($sql);
+//$sql_three = "INSERT INTO userprofiles (userName) VALUES ('".$username."')";
+//$link->query($sql_three);
 
 echo $username;
-//echo $studentName;
 echo $emailAddress;
 echo $password;
 echo $mobileNumber;
-//echo $course;
-//echo $year;
 
 header("location: RegisterSuccess.php");
