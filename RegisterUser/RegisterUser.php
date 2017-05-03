e740d194be593e990b5e439e592e1662a70677f9
jennifergowie <0308182@rgu.ac.uk>
2017-05-03 02:40:36 +0100

    Add files via upload

--- RegisterUser/RegisterUser.php
+++ RegisterUser/RegisterUser.php
@@ -2,7 +2,8 @@
 
 session_start(); // Start Session
 include("../AdminFiles/dbConnect.php");    //Establish database connection
-$username=$_POST["username"]; //Get username that has been entered
+$username=$_POST["username"]; //Get userna
+//me that has been entered
 $studentName=$_POST["studentName"];
 $emailAddress=$_POST["email"]; //Get email address that has been entered
 $password=$_POST["password"]; //Get password that has been entered
@@ -20,6 +21,11 @@ echo $course;
 echo $year;
 
 include("../AdminFiles/dbConnect.php");    //Establish database connection
+
+$sql = "INSERT INTO users (UserName, EmailAddress, Password, MobileNumber) VALUES ('".$username."','".$emailAddress."','".$password."', '".$mobileNumber."')";
+$link->query($sql);
+
+
 $sql = "INSERT INTO userprofiles(UserName, StudentName, EmailAddress, Password, MobileNumber, Course, Year) VALUES ('".$username."','".$studentName."','".$emailAddress."','".$password."','".$mobileNumber."', '".$course."','".$year."')";
 $link->query($sql);
 
