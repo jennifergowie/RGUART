a28ec6b464e3561aa70f7ab0c8a22c9ee78d42f5
jennifergowie <0308182@rgu.ac.uk>
2017-05-03 01:44:36 +0100

    Add files via upload

--- RegisterUser/RegisterUser.php
+++ RegisterUser/RegisterUser.php
@@ -8,12 +8,11 @@ $username=$_POST["username"]; //Get username that has been entered
 $studentName=$_POST["studentname"];
 $emailAddress=$_POST["email"]; //Get email address that has been entered
 $password=$_POST["password"]; //Get password that has been entered
-$mobileNumber=$_POST["phone"]; //Get mobile number that has been entered
+$mobileNumber=$_POST["tel"]; //Get mobile number that has been entered
 $course =$_POST["course"];
 $year=$_POST["year"];
 
-$sql = "INSERT INTO usersprofiles (UserName, StudentName, EmailAddress, Password, MobileNumber, Course, Year) 
-VALUES ('".$username."','".$studentName."',".$emailAddress."','".$password."', '".$mobileNumber."', '".$course.", '".$year."'')";
+$sql = "INSERT INTO usersprofiles (UserName, StudentName, EmailAddress, Password, MobileNumber, Course, Year) VALUES ('".$username."','".$studentName."',".$emailAddress."','".$password."', '".$mobileNumber."', '".$course.", '".$year."'')";
 $link->query($sql);
 
 echo $username;
