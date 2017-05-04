e0b74f95df6769c9e6bf64dcd0a495533148141b
jennifergowie <0308182@rgu.ac.uk>
2017-05-04 23:25:53 +0100

    Add files via upload

--- SearchForRegisteredUser.php
+++ SearchForRegisteredUser.php
@@ -54,10 +54,11 @@ include("dbConnect.php");
         <center><img style="vertical-align: top; display: inline; height: 75px; width: 300px" src="Resources/Images/Robert_Gordon_University.png" alt="Logo"><br/></center>
         <br/>
         <?php
-            if ($profilePicturesResults != null){
-                echo '<img src="data:image/jpeg;base64,'.base64_encode( $profilePicturesResults['content'] ).'" width="100" height="100"/>';
-            ?><br/>
+            if ($profilePicturesResults != null) {
+                echo '<img src="data:image/jpeg;base64,' . base64_encode($profilePicturesResults['content']) . '" width="100" height="100"/>';
             }
+        ?><br/>
+
 
         <span>Student Name: </span><?php echo "<b>{$_SESSION["searchedStudentsName"]}</b>"?><br>
         <span>Course: </span><?php echo "<b>{$_SESSION["searchedStudentsCourse"]}</b>"?><br>
